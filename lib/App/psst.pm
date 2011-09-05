af2d2fe61194130d2ae699c15bc5176868fe59cf
Matthew Astley <beeep$mca@t8o.org>
2011-09-05 09:01:11 +0100

    basic makefile.
    placeholder package;
    blame myself at Sourceforge;
    initial manifest
    the usual ignorance

---
+++ lib/App/psst.pm
@@ -0,0 +1,22 @@
+package App::psst;
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+App::psst - prompt string setting tool
+
+=head1 DESCRIPTION
+
+This package is a placeholder, currently the script does all the work.
+
+=head1 SEE ALSO
+
+L<psst(1)>
+
+=cut
+
+1;
